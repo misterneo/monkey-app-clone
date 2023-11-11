09399436b7b7efb9b92234d1720a642d36173174
misterneo <elmehdi.lahmidi@gmail.com>
2023-11-11 19:04:45 +0100

    added env example file

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,18 +1,18 @@
 import Peer from "peerjs";
 import { createContext } from "react";
 
-export const debugMode = false;
+export const debugMode = import.meta.env.VITE_DEBUG_MODE === "true" || false;
 
 export const VideoProvider = createContext();
 
 export const peer = new Peer();
 
-const SERVER_PORT = 3000;
+const SERVER_PORT = import.meta.env.VITE_SERVER_PORT || 3000;
 const CLIENT_PORT = 5173;
 
-export const WS_URL = window.location.href
-  .replace("http", "ws")
-  .replace(CLIENT_PORT, SERVER_PORT);
+export const WS_URL =
+  import.meta.env.VITE_WS_REMOTE_URL ||
+  window.location.href.replace("http", "ws").replace(CLIENT_PORT, SERVER_PORT);
 
 export const HEARTBEAT = {
   message: "ping",
